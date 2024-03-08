44a3998fa2dc5ce40c9701ea7421b4d6a5241c50
sanhorva <sandor.horvath@zeiss.com>
2024-03-08 15:49:47 +0100

    resource

--- main.tf
+++ main.tf
@@ -20,6 +20,37 @@ resource "azurerm_virtual_network" "mtc-vn" {
   }
 }
 
+resource "azurerm_linux_virtual_machine" "mtc-vm" {
+  name                = "mtc-machine"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.mtc-nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  tags = {
+    environment = "dev"
+  }
+}
+
 resource "azurerm_subnet" "mtc-subnet-1" {
   name                 = "mtc-subnet-1"
   resource_group_name  = azurerm_resource_group.mtc-rg.name
